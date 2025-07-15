9b24643d7ea541a25d271462a789d0dea85f8000
Leonardo Beroes <lberoes@aulasneo.com>
2025-07-15 16:00:01 -0400

    feat: register OpenedXAnalyticsViewSet and OpenedXCourseViewSet in urls

--- openedx_owly_apis/urls.py
+++ openedx_owly_apis/urls.py
@@ -1,10 +1,19 @@
 """
 URLs for openedx_owly_apis.
 """
-from django.urls import re_path  # pylint: disable=unused-import
-from django.views.generic import TemplateView  # pylint: disable=unused-import
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from openedx_owly_apis.views.analytics import OpenedXAnalyticsViewSet
+from openedx_owly_apis.views.courses import OpenedXCourseViewSet
+
+
+
+router = DefaultRouter()
+router.register(r'owly-analytics', OpenedXAnalyticsViewSet, basename='owly-analytics')
+router.register(r'owly-courses', OpenedXCourseViewSet, basename='owly-courses')
+
 
 urlpatterns = [
-    # TODO: Fill in URL patterns and views here.
-    # re_path(r'', TemplateView.as_view(template_name="openedx_owly_apis/base.html")),
+    path('', include(router.urls)),
 ]
