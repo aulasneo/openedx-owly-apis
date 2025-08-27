7b88fd96eee56d599e2d21a3503446f87e1b2b6c
Andrés González <andres@aulasneo.com>
2025-08-27 10:30:18 -0300

    test: add API endpoint tests for course content and settings management

--- tests/test_views_api.py
+++ tests/test_views_api.py
@@ -61,6 +61,118 @@ class TestOpenedXCourseViewSet:
         assert resp.status_code == 200
         assert resp.data["called"] == "update_course_settings_logic"
 
+    def test_create_structure_calls_logic(self, api_factory):
+        from openedx_owly_apis.views.courses import OpenedXCourseViewSet
+        view = OpenedXCourseViewSet.as_view({"post": "create_structure"})
+        req = api_factory.post(
+            "/owly-courses/structure/",
+            {"course_id": "course-v1:ORG+NUM+RUN", "units_config": {"sections": []}, "edit": True},
+            format="json",
+        )
+        user = _auth_user()
+        force_authenticate(req, user=user)
+        resp = view(req)
+        assert resp.status_code == 200
+        assert resp.data["called"] == "create_course_structure_logic"
+
+    def test_add_html_content_calls_logic(self, api_factory):
+        from openedx_owly_apis.views.courses import OpenedXCourseViewSet
+        view = OpenedXCourseViewSet.as_view({"post": "add_html_content"})
+        req = api_factory.post(
+            "/owly-courses/content/html/",
+            {"vertical_id": "block-v1:ORG+NUM+RUN+type@vertical+block@v1", "html_config": {"html": "<p>x</p>"}},
+            format="json",
+        )
+        user = _auth_user()
+        force_authenticate(req, user=user)
+        resp = view(req)
+        assert resp.status_code == 200
+        assert resp.data["called"] == "add_html_content_logic"
+
+    def test_add_video_content_calls_logic(self, api_factory):
+        from openedx_owly_apis.views.courses import OpenedXCourseViewSet
+        view = OpenedXCourseViewSet.as_view({"post": "add_video_content"})
+        req = api_factory.post(
+            "/owly-courses/content/video/",
+            {"vertical_id": "block-v1:ORG+NUM+RUN+type@vertical+block@v1", "video_config": {"url": "http://v"}},
+            format="json",
+        )
+        user = _auth_user()
+        force_authenticate(req, user=user)
+        resp = view(req)
+        assert resp.status_code == 200
+        assert resp.data["called"] == "add_video_content_logic"
+
+    def test_add_problem_content_calls_logic(self, api_factory):
+        from openedx_owly_apis.views.courses import OpenedXCourseViewSet
+        view = OpenedXCourseViewSet.as_view({"post": "add_problem_content"})
+        req = api_factory.post(
+            "/owly-courses/content/problem/",
+            {"vertical_id": "block-v1:ORG+NUM+RUN+type@vertical+block@v1", "problem_config": {"xml": "<problem/>"}},
+            format="json",
+        )
+        user = _auth_user()
+        force_authenticate(req, user=user)
+        resp = view(req)
+        assert resp.status_code == 200
+        assert resp.data["called"] == "add_problem_content_logic"
+
+    def test_add_discussion_content_calls_logic(self, api_factory):
+        from openedx_owly_apis.views.courses import OpenedXCourseViewSet
+        view = OpenedXCourseViewSet.as_view({"post": "add_discussion_content"})
+        req = api_factory.post(
+            "/owly-courses/content/discussion/",
+            {"vertical_id": "block-v1:ORG+NUM+RUN+type@vertical+block@v1", "discussion_config": {"topic": "t"}},
+            format="json",
+        )
+        user = _auth_user()
+        force_authenticate(req, user=user)
+        resp = view(req)
+        assert resp.status_code == 200
+        assert resp.data["called"] == "add_discussion_content_logic"
+
+    def test_configure_certificates_calls_logic(self, api_factory):
+        from openedx_owly_apis.views.courses import OpenedXCourseViewSet
+        view = OpenedXCourseViewSet.as_view({"post": "configure_certificates"})
+        req = api_factory.post(
+            "/owly-courses/certificates/configure/",
+            {"course_id": "course-v1:ORG+NUM+RUN", "certificate_config": {"enabled": True}},
+            format="json",
+        )
+        user = _auth_user()
+        force_authenticate(req, user=user)
+        resp = view(req)
+        assert resp.status_code == 200
+        assert resp.data["called"] == "enable_configure_certificates_logic"
+
+    def test_control_unit_availability_calls_logic(self, api_factory):
+        from openedx_owly_apis.views.courses import OpenedXCourseViewSet
+        view = OpenedXCourseViewSet.as_view({"post": "control_unit_availability"})
+        req = api_factory.post(
+            "/owly-courses/units/availability/control/",
+            {"unit_id": "block-v1:ORG+NUM+RUN+type@sequential+block@u1", "availability_config": {"due": "2024-01-31"}},
+            format="json",
+        )
+        user = _auth_user()
+        force_authenticate(req, user=user)
+        resp = view(req)
+        assert resp.status_code == 200
+        assert resp.data["called"] == "control_unit_availability_logic"
+
+    def test_update_advanced_settings_calls_logic(self, api_factory):
+        from openedx_owly_apis.views.courses import OpenedXCourseViewSet
+        view = OpenedXCourseViewSet.as_view({"post": "update_advanced_settings"})
+        req = api_factory.post(
+            "/owly-courses/settings/advanced/",
+            {"course_id": "course-v1:ORG+NUM+RUN", "advanced_settings": {"key": "value"}},
+            format="json",
+        )
+        user = _auth_user()
+        force_authenticate(req, user=user)
+        resp = view(req)
+        assert resp.status_code == 200
+        assert resp.data["called"] == "update_advanced_settings_logic"
+
 
 class TestOpenedXAnalyticsViewSet:
     def test_overview_calls_logic(self, api_factory):
