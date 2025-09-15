d2e6e988eb274bfa0f25019bfb1ae75b2448da63
gabrielC1409 <gcarvajal@aulasneo.com>
2025-09-15 11:13:17 -0400

    refactor: remove unused authentication and permission imports from config view

--- openedx_owly_apis/views/config_openedx.py
+++ openedx_owly_apis/views/config_openedx.py
@@ -1,20 +1,15 @@
-from edx_rest_framework_extensions.auth.jwt.authentication import JwtAuthentication
-from openedx.core.lib.api.authentication import BearerAuthentication
 from rest_framework import viewsets
-from rest_framework.authentication import SessionAuthentication
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
 # Logic operations for configuration endpoints
 from openedx_owly_apis.operations.config import is_owly_chat_enabled_logic
-from openedx_owly_apis.permissions import IsAdminOrCourseStaff
 
 
 class OpenedXConfigViewSet(viewsets.ViewSet):
     """
     ViewSet para configuracuión de la plataforma Open edX.
     """
-    
     @action(detail=False, methods=['get'], url_path='enable_owly_chat')
     def enable_owly_chat(self, request):
         """Return whether the Owly chat is enabled by waffle flag.
